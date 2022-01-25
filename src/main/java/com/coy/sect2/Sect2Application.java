58b860690526d9bb102eb2a538b13feca57ffec2
fp <huang.fp@gmail.com>
2022-01-25 16:38:10 -0600

    initial commit

---
+++ src/main/java/com/coy/sect2/Sect2Application.java
@@ -0,0 +1,13 @@
+package com.coy.sect2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sect2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sect2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coy/sect2/Sect2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coy.sect2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sect2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
